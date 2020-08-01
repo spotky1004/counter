db23e1492e0ab4334d9b9ee4c11830c5e507c479
Spotky1004 <spotky1004@gmail.com>
2020-08-01 23:49:16 +0900

    0.1

--- counter.js
+++ counter.js
@@ -1,15 +1,29 @@
+function setDigitPos(counterElement, num, digit) {
+  if (num > 1e15) {
+    num = (Number(num).toExponential(digit-6)).toString().replace('+', '');
+    if (num < 1e100) {
+      num = num.replace('e', 'e0');
+    } else if (num < 1e10) {
+      num = num.replace('e', 'e00');
+    }
+  }
+  for (var i = 0; i < digit; i++) {
+    digitThis = document.querySelector(counterElement + ' > .counterNumArea > .counterNumFrame:nth-child(' + (i+1) + ') > div');
+    if (num[i] == 'e') {
+      digitThis.style.top = '-60vh';
+    } else if (num[i] == '.') {
+      digitThis.style.top = '-66vh';
+    } else {
+      digitThis.style.top = '-' + num[i]*6 + 'vh';
+    }
+  }
+}
+
 function counter(counterElement, num, digit) {
   elementThis = document.querySelector(counterElement);
-  num = num.toString();
-  try {
-    numberBefore = document.querySelector(counterElement + ' > .countBefNum').innerText;
-  } catch {
-    befNum = document.createElement('div');
-    befNumText = document.createTextNode('0');
-    befNum.appendChild(befNumText);
-    befNum.className += "countBefNum";
-    befNum.style.cssText = "display: none;";
-    elementThis.appendChild(befNum);
+  num = Math.floor(num).toString();
+  if (digit >= 16) {
+    digit = 16;
   }
   if (num.length != digit) {
     loopCount = digit-num.length;
@@ -18,10 +32,7 @@ function counter(counterElement, num, digit) {
     }
   }
   try {
-    for (var i = 0; i < digit; i++) {
-      digitThis = document.querySelector(counterElement + ' > .counterNumArea > .counterNumFrame:nth-child(' + (i+1) + ') > div');
-      digitThis.style.top = '-' + num[i]*6 + 'vh';
-    }
+    setDigitPos(counterElement, num, digit);
   } catch {
     numArea = document.createElement('div');
     numArea.className += "counterNumArea";
@@ -33,12 +44,11 @@ function counter(counterElement, num, digit) {
       numAreaElement.appendChild(frameThis);
       frameThis = document.querySelector(counterElement + ' > .counterNumArea > .counterNumFrame:nth-child(' + (i+1) + ')');
       digitThis = document.createElement('div');
-      digitTxt = document.createTextNode('0\ \ 1\ \ 2\ \ 3\ \ 4\ \ 5\ \ 6\ \ 7\ \ 8\ \ 9');
+      digitTxt = document.createTextNode('0\ \ 1\ \ 2\ \ 3\ \ 4\ \ 5\ \ 6\ \ 7\ \ 8\ \ 9\ \ E\ \ .');
       digitThis.appendChild(digitTxt);
       digitThis.className += "counterNum";
-      digitThis.style.top = '-' + num[i]*6 + 'vh';
       frameThis.appendChild(digitThis);
     }
+    setDigitPos(counterElement, num, digit);
   }
-  document.querySelector(counterElement + ' > .countBefNum').innerText = num;
 }
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-number = 0;
+number = 1e34;
 function increment(num) {
   number += num;
-  counter('#test-counter', number, 4);
+  counter('#test-counter', number, 20);
 }
 
 window.onload = function() {
-  counter('#test-counter', 0, 4);
+  counter('#test-counter', 0, 20);
+  setInterval( function () {
+    increment(Math.floor(number/1000)+1);
+  }, 33);
 }
